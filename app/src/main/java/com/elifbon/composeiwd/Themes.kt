b72e67d3909e11ebd34b7feaa794c6a0ec8f974d
Elif Boncuk <elifboncuk88@gmail.com>
2020-04-18 21:25:57 +0300

    Add navigation bar

--- app/src/main/java/com/elifbon/composeiwd/Themes.kt
+++ app/src/main/java/com/elifbon/composeiwd/Themes.kt
@@ -4,6 +4,11 @@ import androidx.ui.material.lightColorPalette
 
 val lightThemeColors = lightColorPalette(
     primary = Color(0xFF1EB980),
+    onPrimary = Color.DarkGray,
+    secondary = Color.White,
+    onSecondary = Color.Black,
+    background = Color.DarkGray,
+    onBackground = Color(0xFF1EB980),
     surface = Color.DarkGray,
     onSurface = Color(0xFF1EB980)
 )
