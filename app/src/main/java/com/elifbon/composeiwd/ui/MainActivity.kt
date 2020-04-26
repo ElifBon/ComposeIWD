a229af9532d95f7858faa8900f541cebca3d1c78
Elif Boncuk <elifboncuk88@gmail.com>
2020-04-26 18:30:14 +0300

    Add topbar

--- app/src/main/java/com/elifbon/composeiwd/ui/MainActivity.kt
+++ app/src/main/java/com/elifbon/composeiwd/ui/MainActivity.kt
@@ -3,11 +3,21 @@ package com.elifbon.composeiwd.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.ui.core.Modifier
 import androidx.ui.core.setContent
+import androidx.ui.foundation.Text
+import androidx.ui.layout.Column
+import androidx.ui.layout.Row
+import androidx.ui.layout.padding
 import androidx.ui.material.Surface
+import androidx.ui.material.TopAppBar
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
+import com.elifbon.composeiwd.R
 import com.elifbon.composeiwd.data.getSpeakersWithImagesLoaded
 import com.elifbon.composeiwd.data.speakerInfoList
+import com.elifbon.composeiwd.themeTypography
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,15 +27,24 @@ class MainActivity : AppCompatActivity() {
 
         setContent {
             IWDDemoApp() {
-                Surface {
-                    SpeakerList(speakerInfoList)
+                Column() {
+                    TopAppBar(content =
+                    { Row() {
+                        Text (text = resources.getString(R.string.app_name),
+                            modifier = Modifier.padding(start = 16.dp, top = 8.dp, end = 16.dp, bottom = 8.dp),
+                        style = themeTypography.h5)
+                    }})
+                    /*Surface {
+                         SpeakerList(speakerInfoList)
 
+                    }*/
+
+                    Surface  {
+                        StateSampleScreen()
+
+                    }
                 }
-
-               /* Surface  {
-                     StateSampleScreen()
-
-                }*/
+               
 
             }
         }
@@ -35,9 +54,10 @@ class MainActivity : AppCompatActivity() {
 @Preview
 @Composable
 fun defaultPreview(){
+
     IWDDemoApp() {
         Surface {
-            SpeakerInformation(speakerInfoList[2])
+            Greeting(name = "Elif")
 
         }
 
